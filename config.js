35a2e8a567fdd09811ca418d6c6e603c4da68dab
Thanujadilsan55 <nimalrathnasiri143@gmail.com>
2025-04-27 06:56:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,4 +9,5 @@ SESSION_ID: process.env.SESSION_ID,
 ALIVE_IMG:process.env.ALIVE_IMG || "https://avatars.gtihubusercontent.com/u/106251140?v=4",
 ALIVE_MSG:process.env.ALIVE_MSG ||"HELLO, I AM DILSAN34-MD I AM ALIVE NEW! !",
     
+    
 };
